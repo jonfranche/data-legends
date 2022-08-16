02c1cc6eb29659a5f098bf4b4a451a920ab09eb2
jonfranche <jonfranche@gmail.com>
2022-08-16 10:45:42 -0700

    adjusted style of NavBar and SubNavBar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,19 +1,48 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "./Navbar.css"
+import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <nav className="nav-bar">
-            
-            <ul className="nav-list">
-                <li className="title"><NavLink to="/">Data Legends</NavLink></li>
-                <li className="nav-item"><NavLink to="/lol" >League of Legends</NavLink></li>
-                <li className="nav-item"><NavLink to="/tft">TFT</NavLink></li>
-                <li className="nav-item"><NavLink to="/about">About</NavLink></li>
-            </ul>
-        </nav>
-    )
-}
+  let activeClassName = "nav-item-active";
+  let inActiveClassName = "nav-item";
+  return (
+    <nav className="nav-bar">
+      <ul className="nav-list">
+        <li className="nav-list-item">
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? activeClassName : inActiveClassName)}
+          >
+            Data Legends
+          </NavLink>
+        </li>
+        <li className="nav-list-item">
+          <NavLink
+            to="/lol"
+            className={({ isActive }) => (isActive ? activeClassName : inActiveClassName)}
+          >
+            League of Legends
+          </NavLink>
+        </li>
+        <li className="nav-list-item">
+          <NavLink
+            to="/tft"
+            className={({ isActive }) => (isActive ? activeClassName : inActiveClassName)}
+          >
+            TFT
+          </NavLink>
+        </li>
+        <li className="nav-list-item">
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? activeClassName : inActiveClassName)}
+          >
+            About
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
